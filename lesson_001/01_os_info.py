43b9f02607e082f4cdfb1a1d6f76e7db9935bb70
Zuza <kirill.dudarev.20020523@gmail.com>
2023-06-16 17:28:00 +1000

    версия операционной системы

---
+++ lesson_001/01_os_info.py
@@ -0,0 +1,13 @@
+# -*- coding: utf-8 -*-
+
+# Нужно собрать информацию об операционной системе и версии пайтона
+
+# TODO запустить этот скрипт закомитить результат его работы (файл os_info.txt)
+
+import os, platform, sys
+
+info = 'OS info is \n{}\n\nPython version is {} {}'.format(platform.uname(), sys.version, platform.architecture())
+print(info)
+
+with open('os_info.txt', 'w') as ff:
+    ff.write(info)
