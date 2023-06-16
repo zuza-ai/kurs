96b148636f618891c265256e573986b8409242cb
Zuza <kirill.dudarev.20020523@gmail.com>
2023-06-16 17:35:41 +1000

    исправлен import

--- lesson_001/01_os_info.py
+++ lesson_001/01_os_info.py
@@ -2,7 +2,7 @@
 
 # Нужно собрать информацию об операционной системе и версии пайтона
 
-import os, platform, sys
+import platform, sys
 
 info = 'OS info is \n{}\n\nPython version is {} {}'.format(platform.uname(), sys.version, platform.architecture())
 print(info)
